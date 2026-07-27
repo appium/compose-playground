073d52c6580df1bb621c6bbb0ffc0c038dc4c3af
Mykola Mokhnach <mokhnach@gmail.com>
2026-07-27 20:33:40 +0200

    chore: Update various Android dependencies (#32)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "io.appium.composeplayground"
-    compileSdk = 35
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "io.appium.composeplayground"
